208cd49b1adb43d6f6eef52ad86ca56c8ded55ab
ailopez- <aitorlpzaudikana@gmail.com>
2022-10-02 01:28:29 +0200

    printfs

--- src/client.c
+++ src/client.c
@@ -15,63 +15,45 @@
 #include <stdio.h>
 #include <time.h>
 
-int	semaforo;
 int server_pid;
-int	num_ack;
+int	signal_timeouts;
 
 void clean_semaforo(int sig, siginfo_t *si, void *uap)
 {
 	if (sig == SIGUSR1)
-	{
-		semaforo = 0;
-		num_ack ++;
-		//write(1, ".", 1);
-	}
+		return;
 }
 
 int	send_byte(char byte, int pid)
 {
 	int i;
-	int timeout;
-	char	temp[2];
 
-	// temp[0] = byte;
-	// temp[1] = '\0';
-	// ft_putstr_fd("Caracter [", 1);
-	// ft_putstr_fd(temp, 1);
-	// ft_putstr_fd("]\n", 1);	
-	ft_putchar_fd(byte, 1);
+	//ft_putchar_fd(byte, 1);
 	i = 0;
 	while (i < 8)
 	{
-		semaforo = 1;
 		if (byte & 0x80)
 			kill (pid, SIGUSR2);
 		else
 			kill (pid, SIGUSR1);
 		byte <<= 1;
 		i++;
-		timeout = 2000;
-		while (semaforo || timeout == 0)
-		{
-			timeout--;
-			//pause();
-		}
-		//usleep(400);
+		usleep(10000);
 	}
-
 	return (0);
 }
 
 void	print_resume(char *str, int pid, int ms)
 {
-	ft_putstr_fd("\n\n 📟 Sended ", 1);
-	ft_putstr_fd(ft_itoa(ft_strlen(str)), 1);
-	ft_putstr_fd(" bytes to PID [" , 1);
-	ft_putstr_fd(ft_itoa(server_pid), 1);
-	ft_putstr_fd("] in " , 1);
-	ft_putstr_fd(ft_itoa(ms), 1);
-	ft_putstr_fd("ms \n\n" , 1);	
+	float errors;
+
+	errors = signal_timeouts * 100;
+	errors = errors / (ft_strlen(str) * 8);
+
+	//ft_printf("\n\n Sended %d bytes to PID [%s]\n\n", 27654, "Hola");
+
+
+	printf("\n\n Sended %d bytes to PID [%d] in %d ms with %f%% timeouts \n\n", ft_strlen(str), pid, ms, errors);
 }
 
 
@@ -91,12 +73,13 @@ int main (int argv, char **argc)
 	server_pid = ft_atoi(argc[1]);
 	t = clock();
 	i = 0;
+	signal_timeouts = 0;
 	while	(argc[2][i])
 		send_byte(argc[2][i++], server_pid);
 	send_byte(argc[2][i], server_pid);
 	t = clock() - t;
 	time = ((double)t) / CLOCKS_PER_SEC;	
-	print_resume (argc[2], server_pid, 0);
+	print_resume (argc[2], server_pid, time * 1000);
 	return (0);
 }
 
--- src/server.c
+++ src/server.c
@@ -14,92 +14,76 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define MAX_NUM_CLIENTS 50
-
 typedef struct s_client{
 	char	*buffer;
 	char	byte;
 	char	num_bit;
 	int		pid;
+	int		byte_recived;
 }	t_client;
 
 t_client	client;
 
-void	print_msj(char *str, int pid)
-{
-	ft_printf("\n\n 📟 Message finished from PID[%d]\n\n", ft_itoa(pid));
+    /* 
+	Sigaction is a struct describing a signal handler. It contains:
+	   - A signal handler function
+	   - A signal mask which specifies which signals should be blocked
+         while the signal handler function is running. If the signal
+         handler returns normally, the original signal mask will be
+         restored. To understand signal masks, see sigprocmask.c
+	   - A set of flags.
+	
+	Specify the signal handler function to be called when one of
+	the specified signals occur.
 
-	//ft_putstr_fd("\n\n 📟 Message finished from PID[", 1);
-	//ft_putstr_fd(ft_itoa(pid), 1);
-	//ft_putstr_fd("]\n\n ", 1);		
-	//ft_putstr_fd("] >> ", 1);	
-	//ft_putstr_fd(str, 1);
-	//ft_putstr_fd("\n", 1);		
+		>> signal.sa_sigaction = signal_recived;
+
+	Add all of the signals to the signal mask set. This means that
+	all signals will be blocked (i.e., delayed) while our signal
+	handler function is running. The original signal mask will be
+	restored when our signal handler exits normally.
+	
+		>> sigfillset(&signal.sa_mask);
+
+ 	If the signal handler gets run in the middle of some function
+	calls (such as open(), read(), or write()) and the signal
+	handler returns normally, there are couple of options: return a
+	failure code EINTR from those functions, or try to resume the
+	function. With SA_RESTART, the function is resumed instead of
+	returning an error. 
+	
+		>> signal.sa_flags = SA_RESTART;
+
+	*/
+
+void	print_msj(int bytes_recived, int pid)
+{
+	ft_printf("\n\n 📟 Message finished from PID[%d]\n", pid);
+	ft_printf(" 🔸 %d bytes recived\n\n", bytes_recived);		
 }
 
 void	reset_client(int pid)
 {
-
-	ft_printf("\n\n 📥 Starting new message from PID [%d]\n\n", ft_itoa(pid));
-
-	//char 	*msg = "\n\n 📥 Starting new message from PID [";
-
-	//ft_putstr_fd(msg, 1);
-	//ft_putstr_fd(ft_itoa(pid), 1);
-	//ft_putstr_fd("]\n\n", 1);
+	ft_printf("\n\n 📥 Starting new message from PID [%d]\n\n", pid);
 	client.pid = pid;
 	client.num_bit = 0;
+	client.byte_recived = 0;
 	free (client.buffer);
 	client.buffer = ft_strdup("");
 }
 
 void signal_recived(int sig, siginfo_t *si, void *uap)
 {
-	//int		id_client;
-	//char	temp[2];
-	//char 	*new_buffer;
-	
-/*
-  	printf ("Signal ");
-    if ( si->si_signo == SIGUSR2)
-         printf ("[SIGUSR2] >> BIT_1");
-    if ( si->si_signo == SIGUSR1)
-         printf ("[SIGUSR1] >> BIT_0");
-    printf (" from PID: %d\n", si->si_pid);   
-*/
-
-	if (si->si_pid == 0)
-	{
-		// printf("signal %d received with siginfo_t:\n", sig);
-		// printf("\tsignal number: si_signo=%d\n", si->si_signo);
-		// printf("\terror number: si_errno=%d\n", si->si_errno);
-		// printf("\tsignal code: si_code=%d\n", si->si_code);
-		// printf("\tsignal value union: si_value, sival_int=%d\n", si->si_value.sival_int);
-		// printf("\tsignal value union: si_value, sival_ptr=%p\n", si->si_value.sival_ptr);
-		// printf("\tsending process ID: si_pid=%d\n", si->si_pid);
-		// printf("\tsending process user's ID: si_uid=%d\n", si->si_uid);
-		// printf("\tfaulting instruction at: si_addr=%p\n", si->si_addr);
-		// printf("\texit value or signal: si_status=%d\n", si->si_status);
-		// printf("\tband event for SIGPOLL: si_band=%ld\n", si->si_band);
-		// printf("\n");
-	}
-
 	if (client.pid != si->si_pid && si->si_pid != 0)
 		reset_client (si->si_pid);
 	client.num_bit++;
 	client.byte |= (sig == SIGUSR2);
-	//printf ("Client PID [%d] - Byte [0x%02x]\n", client.pid, client.byte);
 	if (client.num_bit == 8)
 	{
 		ft_putchar_fd(client.byte, 1);
-		//printf ("BYRE READY [%c] From PID [%d]\n Id [%d]", client.byte, client.pid, id_client);
-		// temp[0] = client.byte;
-		// temp[1] = '\0';
-		// new_buffer = ft_strjoin(client.buffer, temp);
-		// free (client.buffer);
-		// client.buffer = new_buffer;
-		// if(client.byte == '\0')
-		// 	print_msj(client.buffer, client.pid);
+		if (client.byte == 0)
+			print_msj(client.byte_recived, client.pid);
+		client.byte_recived++;
 		client.byte = 0;	
 		client.num_bit = 0;
 	}
@@ -110,42 +94,14 @@ void signal_recived(int sig, siginfo_t *si, void *uap)
 
 int main (void)
 {
-	//int id_client;
-
-    /* sigaction is a struct describing a signal handler. It contains:
-	   - A signal handler function
-	   
-	   - A signal mask which specifies which signals should be blocked
-         while the signal handler function is running. If the signal
-         handler returns normally, the original signal mask will be
-         restored. To understand signal masks, see sigprocmask.c
-	   
-	   - A set of flags.
-	*/   
     struct sigaction signal;
-	/* Specify the signal handler function to be called when one of
-	 * the specified signals occur. */
-    signal.sa_sigaction = signal_recived;
-	/* Add all of the signals to the signal mask set. This means that
-	   all signals will be blocked (i.e., delayed) while our signal
-	   handler function is running. The original signal mask will be
-	   restored when our signal handler exits normally. */
-	sigfillset(&signal.sa_mask);
- 	/* If the signal handler gets run in the middle of some function
-	 * calls (such as open(), read(), or write()) and the signal
-	 * handler returns normally, there are couple of options: return a
-	 * failure code EINTR from those functions, or try to resume the
-	 * function. With SA_RESTART, the function is resumed instead of
-	 * returning an error. */
-	signal.sa_flags = SA_RESTART;
 
-    /* Add our signals handler */
+    signal.sa_sigaction = signal_recived;
+	sigfillset(&signal.sa_mask);
+	signal.sa_flags = SA_RESTART;
     sigaction(SIGUSR2, &signal, NULL);
     sigaction(SIGUSR1, &signal, NULL);
-
-	ft_putstr_fd("\nServer PID [", 1);
-	ft_putstr_fd(ft_itoa(getpid()), 1);
-	ft_putstr_fd("]\n\n", 1);
+	ft_printf("\nServer PID [%d]\n\n", getpid());
     while(1)
 	{
 		pause();
